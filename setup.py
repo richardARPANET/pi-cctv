f6c095327136aa614551fe841cc470a1c042899e
richard <richard@richard.do>
2014-08-15 23:29:22 +0100

    CI

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 import os
 from setuptools import setup, find_packages
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='picctv',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
@@ -17,7 +15,6 @@ setup(
         'console_scripts': ['picctv = picctv.picctv:main']
     },
     description='CCTV for the RaspberryPi & Camera Module.',
-    long_description=README,
     author='Richard O\'Dwyer',
     author_email='richard@richard.do',
     url='https://github.com/richardasaurus/pi-cctv',
